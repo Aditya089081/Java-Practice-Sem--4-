b68841224ee6eeb5f6409fa029432a73606f7477
Aditya089081 <aktiwari089081@gmail.com>
2025-02-05 03:58:48 +0530

    Day 3

---
+++ MyApp/src/App.java
@@ -0,0 +1,7 @@
+public class App {
+    public static void main(String[] args) throws Exception {
+        if(3>1){
+            System.out.println("lund kya karu");
+        }
+    }
+}
--- if_else.java
+++
@@ -1,5 +0,0 @@
-public class if_else {
-    public static void main(string[] arg){
-        system.out.println("Hellow World");
-    }
-}
