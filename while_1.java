4734a14af6bf3d909c945102f286e7758d9cd4ec
Aditya089081 <aktiwari089081@gmail.com>
2025-02-23 13:41:18 +0530

    Exploring java

---
+++ while_1.java
@@ -0,0 +1,20 @@
+public class while_1 {
+    // Printing Stars
+    /**Welcome to Aditya's World.
+     * @param arg
+     */
+    public static void main(String[] arg){
+        int a = 5;
+        int i =1;
+        while(i<=a){
+                int j = 0;
+                while(j<i){
+                    System.out.print("* ");
+                    j++;
+                }
+            i++;
+            System.out.print("\n");
+        }
+
+    }
+}
