e74b419b21f1b0cc3d408a85bf08174784ac5000
Aditya089081 <aktiwari089081@gmail.com>
2025-02-27 23:55:22 +0530

    Exploring Java

--- Practice_5.java
+++ Practice_5.java
@@ -1,6 +1,6 @@
 
 // 
-// Create a program using dæwhile to implement a number guessing game.
+// 
 // Create a program using for loop multiplication table for a number.
 // Create a program using for to display if a number is prime or not.
 // Create a program using for-each to find the maximum value in an integer array.
@@ -33,13 +33,24 @@ import java.util.Scanner;
 public class Practice_5 {
     // 1.  Create a program using do-while to find password checker until a valid
 // password is entered.
-public static void main(String[] arg){
-    Scanner input = new Scanner(System.in);
-    int password;
-    do{
-        password = input.nextInt();
-    }while(password!=1234);
-    System.out.println("Correct Password Entered.");
-    input.close();
-}
+// public static void main(String[] arg){
+//     Scanner input = new Scanner(System.in);
+//     int password;
+//     do{
+//         password = input.nextInt();
+//     }while(password!=1234);
+//     System.out.println("Correct Password Entered.");
+//     input.close();
+// }
+
+    // 2.  Create a program using dæwhile to implement a number guessing game.
+
+    public static void main(String[] arg){
+        Scanner input = new Scanner(System.in);
+        int num;
+        do{
+            num = input.nextInt();
+        }while(num != -1);
+        input.close();
+    }
 }
